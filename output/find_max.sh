763af096e594324b772874adbee47b985d62df2e
mru <mru@sisyphus.teil.cc>
2011-05-23 22:32:06 +0200

    first parameter study - results

--- output/find_max.sh
+++ output/find_max.sh
@@ -1,11 +1,14 @@
 #! /bin/bash -e
 
+# mru, 2011-05
+#
+# prints the parameter set with the highest f-score
 
 for i in train*.onsets.paramstudy.eval; do 
 awk -f - $i <<"EOF"
 
 BEGIN { max = 0.0; }
-{ if ($10 > max && $10 != "NaN" && $10 != "FMEASURE") {max = $10; field = $0;}; }
+NR > 1 { if ($10 > max && $10 != "NaN") {max = $10; field = $0;}; }
 END { print FILENAME ": " field; }
 EOF
 
