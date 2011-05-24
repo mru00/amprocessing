7d9f52a401cc629cd0e2630e9e07cd9087c74e0d
mru <mru@sisyphus.teil.cc>
2011-05-24 23:31:19 +0200

    fixed bug in alg3; added files for torque

--- output/plot_eval_grouped_by_file.sh
+++ output/plot_eval_grouped_by_file.sh
@@ -1,33 +1,28 @@
 #!/bin/bash -xe
 
-#./find_max.sh > max.onsets.paramstudy.eval
-
 
 {
-awk -f - max.onsets.paramstudy.eval <<"EOF"
+awk -f - <(./find_best_parameters_by_algorithm) <<"EOF"
 
 /train...onsets.paramstudy...eval/ { 
 
-file = substr($0, 6,2);
-alg = substr($0, 27,1);
-fmeas = $11
-if (!fmeas) { fmeas = "-" }
+  file = substr($0, 6,2);
+  alg = substr($0, 27,1);
+  fmeas = $11
+  if (!fmeas) { fmeas = "-" }
 
-#print file " " alg " " fmeas
-
-if (file in data) {
-  data[file] = data[file] " "
-}
-data[file] = data[file] fmeas
+  if (file in data) {
+    data[file] = data[file] " "
+  }
+  data[file] = data[file] fmeas
 }
 
 END {
-for (i = 1; i <= 20; i ++ ) {
-  ind = sprintf("%02d", i)
-  print ind " " data[ind]
+  for (i = 1; i <= 20; i ++ ) {
+    ind = sprintf("%02d", i)
+    print ind " " data[ind]
+  }
 }
-}
-
 EOF
 }  > grouped_by_file.eval
 
@@ -37,11 +32,10 @@ EOF
   gnuplot -p <<"EOF"
 set terminal png size 1024,768
 set style data histogram
-set title "F-Measure for every algorithm grouped by file, autotuned parameters"
+set title "F-Measure for every algorithm grouped by file, parameters autotuned per file"
 set style histogram clustered gap 5
 set style fill solid border -1
 set datafile missing "-"
-
 
 plot for [i=2:8] "grouped_by_file.eval" u i t ""
 
